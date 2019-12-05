3d1648e85849ac28a2dbbb0e81f00716aefbef76
pwojczyn <pwojczyn@gmail.com>
2019-12-05 18:30:06 +0100

    update 05-12-2019

--- src/app.js
+++ src/app.js
@@ -1,25 +1,28 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Andrew Mead'
-    })
-})
+// app.get('', (req, res) => {
+//     res.render('index', {
+//         title: 'Weather',
+//         name: 'Andrew Mead'
+//     })
+// })
 
 app.get('/about', (req, res) => {
     res.render('about', {
@@ -30,7 +33,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.'
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
     })
 })
 
